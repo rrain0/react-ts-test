c96d69b1a74e5ca7980f8c669c39c8cfbdd589d9
rrain <rrainpath@gmail.com>
2025-01-11 14:47:33 +0800

    refactor

--- src/EXAMPLES/TypeScriptTest/TYPESCRIPT-TEST.ts
+++ src/EXAMPLES/TypeScriptTest/TYPESCRIPT-TEST.ts
@@ -96,12 +96,10 @@ export function typescriptTest(){
   tupleTest()
   keyofTest()
   indexedTypes()
-  conditionalTypesTest()
   mappedTypesTest()
   readonlyOptionalConstLateinitSatisfiesAutoaccessor()
   interfaceTest()
   discriminatoryAssociationTest()
-  constructorInInterfaceTest()
   functionsTest()
   functionParametersTest()
   promiseTypes()
@@ -904,97 +902,7 @@ export function indexedTypes(){
 }
 
 
-export function conditionalTypesTest(){
-  // https://www.typescriptlang.org/docs/handbook/2/conditional-types.html
-  
-  interface Animal { live(): void }
-  interface Dog extends Animal { woof(): void }
-  
-  type Example1 = Dog extends Animal ? number : string;
-  // Example1 is: type Example1 = number
-  type Example2 = RegExp extends Animal ? number : string;
-  // Example2 is: type Example2 = string
-  
-  
-  
-  interface IdLabel { id: number /* some fields */ }
-  interface NameLabel { name: string /* other fields */ }
-  
-  // function overload
-  // объявления перегруженной функции
-  function createLabel(id: number): IdLabel
-  function createLabel(name: string): NameLabel
-  // сама функция: параметры могут быть any или ...args: any[]
-  function createLabel(nameOrId: any): IdLabel | NameLabel { throw "unimplemented" }
-  {
-    const n = createLabel(777)
-    const s = createLabel('string')
-  }
-  
-  // объявления можно сократить до:
-  type NameOrId<T extends number | string> = T extends number ? IdLabel : NameLabel;
-  function createLabel2<T extends string | number>(nameOrId: T): NameOrId<T> { throw "unimplemented";}
-  
-  
-  // function type
-  type UserGenerator0 = (username: string, email?: string) => string;
-  // function type overloading
-  type UserGenerator1 = {
-    (username: string): string;
-    (username: string, email?: string): string;
-  };
-  
-  
-  
-  
-  // Infer type from context
-  // если Type это массив, то Item становится типом элементов этого массива
-  type Flatten<Type> = Type extends Array<infer Item> ? Item : Type;
-  // Получить тип возвращаемого значения функции (если Fun это функция)
-  type GetReturnType<Fun> = Fun extends (...args: never[]) => infer Return ? Return : never
-  
-  type Num = GetReturnType<() => number>; // number
-  type Str = GetReturnType<(x: string) => string>; // string
-  type Bools = GetReturnType<(a: boolean, b: boolean) => boolean[]>; // boolean[]
-  
-  // when typeof overloading function, it gets LAST overload declaration
-  type T1 = ReturnType<typeof createLabel> // IdLabel | NameLabel
-  
-  
-  
-  // Distributive Conditional Types
-  type ToArray<Type> = Type extends any ? Type[] : never;
-  type StrArrOrNumArr = ToArray<string | number>; // string[] | number[]
-  
-  type ToArrayNonDist<Type> = [Type] extends [any] ? Type[] : never;
-  type StrArrOrNumArrNotDist = ToArrayNonDist<string | number>; // type StrArrOrNumArr = (string | number)[]
-  
-  
-  
-  
-  {
-    // ts4.7 extends Constraints on infer Type Variables
-    
-    // Вернуть тип строки первого элемента, если это строка, иначе never:
-    type FirstIfString1<T> = T extends [infer S, ...unknown[]]
-      ? S extends string ? S : never
-      : never
-    
-    type FirstIfString2<T> = T extends [string, ...unknown[]]
-      ? T[0]
-      : never
-    
-    type FirstIfString3<T> = T extends [infer S extends string, ...unknown[]]
-      ? S
-      : never
-    
-    type A = FirstIfString3<[string, number, number]> // A is string
-    type B = FirstIfString3<['hello', number, number]> // B is 'hello'
-    type C = FirstIfString3<['hello'|'world', boolean]> // C is 'hello'|'world'
-    type D = FirstIfString3<[boolean, string, string]> // D is never
-    type E = FirstIfString3<[number|string, ...string[]]> // E is never
-  }
-}
+
 
 
 export function mappedTypesTest() {
@@ -1204,23 +1112,9 @@ export function readonlyOptionalConstLateinitSatisfiesAutoaccessor(){
 }
 
 
-export function conditionalTypes(){
-  
-  const f = (c?: number | undefined) => {
-    if (c) return {a: c}
-    else return {b: 1}
-  }
-  
-  const c1 = undefined
-  const obj = f(c1) as typeof c1 extends undefined ? {b: 1} : {a: typeof c1} // typeof obj is {b: 1}
-  //obj.a = 8 // not permitted but b in this case can't be undefined
-}
 
 
-
-
-
-export function interfaceTest(){
+export function interfaceTest() {
   // Интерфейсы
   
   // instanceof не применимо к интерфейсам - интерфейс не компилируется в код
@@ -1409,34 +1303,6 @@ export function discriminatoryAssociationTest() {
 }
 
 
-export function constructorInInterfaceTest() {
-  interface ClockInterface {
-    tick(): void
-  }
-  // любой объект, у которого парметры конструктора такие - подходит под этот интерфейс
-  // нельзя просто реализовать этот интерфейс, т.к. constructor находится в статической составляющей класса,
-  // а не в экземпляре класса, и он не включён в проверку
-  // объект класса потом уже имеет конструктор
-  interface ClockConstructor {
-    new (hour: number, minute: number): ClockInterface
-  }
-  
-  class DigitalClock implements ClockInterface {
-    constructor(h: number, m: number) { }
-    tick(){ console.log("beep beep") }
-  }
-  class AnalogClock implements ClockInterface {
-    constructor(h: number, m: number) { }
-    tick(){ console.log("tick tock") }
-  }
-  
-  function createClock(constructr: ClockConstructor, hour: number, minute: number): ClockInterface {
-    return new constructr(hour, minute)
-  }
-  
-  let digitalClock: DigitalClock = createClock(DigitalClock, 6, 43)
-  let analogClock: AnalogClock = createClock(AnalogClock, 6, 43)
-}
 
 
 
